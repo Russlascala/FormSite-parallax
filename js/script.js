1322e34db2a8e733c26d64c703a56f0a5c39c300
Russ Lascala <russlascala@gmail.com>
2018-09-10 14:26:00 -0400

    Added all styles to the page

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// Script to animate the Text Sections and button under form
+$(window).resize(function () {
+    var width = $(window).width();
+    var height1 = $(".view").height();
+    if (width > 752) {
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > (height1 / 2)) {
+                $('.bounceTrigger').addClass(' animated bounceIn');
+            } 
+        });
+    } else if (width <= 752) {
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > height1 ) {
+                $('.bounceTrigger').addClass(' animated bounceIn');
+            }
+        });
+    }
+})
+    .resize();//trigger the resize event on page load.
+
+    
+// Script to animate the three over on the image
+$(window).resize(function () {
+    var width = $(window).width();
+    var height2 = $(".sectionScroll").height();
+    if (width > 752) {
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > height2) {
+                $('.slideRight1').addClass('animated  slideInRight');
+                $('.slideRight2').addClass('animated  slideInRight');
+                $('.slideLeft').addClass('animated  slideInLeft');
+            }
+        });
+    } else if (width < 752) {
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > (height2 * 1.5 )) {
+                $('.slideRight1').addClass('animated  slideInRight');
+                $('.slideRight2').addClass('animated  slideInRight');
+                $('.slideLeft').addClass('animated  slideInLeft');
+            }
+        });
+    }
+})
+    .resize();//trigger the resize event on page load.
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function () {
+            if (window.scrollY != 0) {
+                window.scrollBy(0, scrollStep);
+            }
+            else clearInterval(scrollInterval);
+        }, 15);
+}

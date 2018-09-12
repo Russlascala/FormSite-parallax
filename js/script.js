70dde6088d993c42abe4bcc94302586dd2e7c7eb
Russ Lascala <russlascala@gmail.com>
2018-09-12 15:08:40 -0400

    Styled the submit page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
+/* Top Month Script */
 /* Month Array */
 const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
 ];
-/* Gets Date and month and year */
+/* Gets month and year */
 const d = new Date();
 var month = monthNames[d.getMonth()];
 var year = d.getFullYear();
-/* inputs the value on top of page  */
+/* inputs the month and year value on top of page  */
 document.getElementById("dateTop").innerHTML = (month + " " + year);
 
 

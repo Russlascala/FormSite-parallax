cee7e14ebbfc7fe4a69435102154580095fc35c1
Russ Lascala <russlascala@gmail.com>
2018-09-13 11:49:47 -0400

    changed copy for mobile and desktop

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
 /* Gets month and year */
 const d = new Date();
 var month = monthNames[d.getMonth()];
+console.log([d.getMonth()]);
 var year = d.getFullYear();
 /* inputs the month and year value on top of page  */
 document.getElementById("dateTop").innerHTML = (month + " " + year);
